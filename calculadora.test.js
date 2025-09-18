cfe9b4ea9085a1d0bac971d508a4febffad7462c
Alejandro-Guerra <l21530285@cancun.tecnm.mx>
2025-09-18 16:39:15 -0500

    actu repo

--- calculadora.test.js
+++ calculadora.test.js
@@ -4,7 +4,7 @@ test('suma 1 + 2 para que sea igual a 3', () => {
   expect(sumar(1, 2)).toBe(3);
 });
 
-// Esta prueba fallará a propósito para que puedas obtener la captura del pipeline fallido
+
 test('suma 5 + 5 para que sea igual a 10', () => {
-  expect(sumar(5, 5)).toBe(11); // Error intencional
+  expect(sumar(5, 5)).toBe(10); 
 });
